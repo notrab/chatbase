92ff53be8dba8a88bb936c86fca5b88b028934a7
Jamie Barton <jamie@notrab.dev>
2023-03-03 21:46:03 +0000

    add github to image domains safelist

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["https://avatars.githubusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
