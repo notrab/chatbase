76dd7e485784f16fc5211cf9b403427bb2588dae
Jamie Barton <jamie@notrab.dev>
2023-05-16 14:10:06 +0100

    move timestamp

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -7,7 +7,6 @@ export type Message = {
   username: string;
   avatar?: string;
   body: string;
-  likes: number;
   createdAt: string;
 };
 
@@ -20,46 +19,52 @@ export const Message = ({ message }: Props) => {
 
   return (
     <div
-      className={`flex relative space-x-1 ${
-        message.username === session?.username
-          ? "flex-row-reverse space-x-reverse"
-          : "flex-row"
+      className={`flex flex-col relative space-x-1 space-y-1 ${
+        message.username === session?.username ? "text-right" : "text-left"
       }`}
     >
-      {message?.avatar && (
-        <div className="w-12 h-12 overflow-hidden flex-shrink-0 rounded">
-          <a
-            href={`https://github.com/${message.username}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              width={50}
-              height={50}
-              src={message.avatar}
-              alt={message.username}
-              title={message.username}
-            />
-          </a>
-        </div>
-      )}
-      <span
-        className={`inline-flex rounded space-x-2 items-start p-3 text-white ${
+      <div
+        className={`flex relative space-x-1 ${
           message.username === session?.username
-            ? "bg-[#4a9c6d]"
-            : "bg-[#363739]"
-        } `}
+            ? "flex-row-reverse space-x-reverse"
+            : "flex-row"
+        }`}
       >
-        {message.username !== session?.username && (
-          <span className="font-bold">{message.username}:&nbsp;</span>
+        {message?.avatar && (
+          <div className="w-12 h-12 overflow-hidden flex-shrink-0 rounded">
+            <a
+              href={`https://github.com/${message.username}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                width={50}
+                height={50}
+                src={message.avatar}
+                alt={message.username}
+                title={message.username}
+              />
+            </a>
+          </div>
         )}
-        <div className="space-y-1">
-          <p className="max-w-sm">{message.body}</p>
-          <p className="text-xs text-white/50">
-            {formatRelative(new Date(message.createdAt), new Date())}
-          </p>
-        </div>
-      </span>
+        <span
+          className={`inline-flex rounded space-x-2 items-start p-3 text-white ${
+            message.username === session?.username
+              ? "bg-[#4a9c6d]"
+              : "bg-[#363739]"
+          } `}
+        >
+          {message.username !== session?.username && (
+            <span className="font-bold">{message.username}:&nbsp;</span>
+          )}
+          <div className="space-y-1">
+            <p className="max-w-sm">{message.body}</p>
+          </div>
+        </span>
+      </div>
+      <p className="text-xs text-white/50">
+        {formatRelative(new Date(message.createdAt), new Date())}
+      </p>
     </div>
   );
 };
